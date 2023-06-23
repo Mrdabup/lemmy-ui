5d7a85c33050eb03c6db02ed83bfbda648407e23
Mrdabup <dabup123456@gmail.com>
2023-06-23 13:10:29 -0400

    Fixed package.json issue when "Git: yarn start (version)" warning shows up
    Pure darkness WIP
    Added "pure-darkness.css" to
    "build-themes-list.ts"

--- src/server/utils/build-themes-list.ts
+++ src/server/utils/build-themes-list.ts
@@ -4,7 +4,13 @@ import { readdir } from "fs/promises";
 const extraThemesFolder =
   process.env["LEMMY_UI_EXTRA_THEMES_FOLDER"] || "./extra_themes";
 
-const themes = ["darkly", "darkly-red", "litely", "litely-red"];
+const themes = [
+  "pure-darkness",
+  "darkly",
+  "darkly-red",
+  "litely",
+  "litely-red",
+];
 
 export async function buildThemeList(): Promise<string[]> {
   if (existsSync(extraThemesFolder)) {
